27a92a7d7baa1e001982b9dc90d0970371808d56
thetnaingtun3 <thetnaingtun.codex@gmail.com>
2021-01-10 12:47:00 +0630

    le

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/','FrontpageController@home');
-// Route::post('/orders','OrderController@store');
-// Route::get('/orders','OrderController@index');
-// Route::get('/items/{id}','OrderController@items');
-// Auth::routes();
+Route::get('/','FrontpageController@home');
+Route::post('/orders','OrderController@store');
+Route::get('/orders','OrderController@index');
+Route::get('/items/{id}','OrderController@items');
+Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
